3dda4937879b260e3382af0cf93820973a660808
roamstory <kokoru58@naver.com>
2018-01-09 21:03:09 +0900

    컴포넌트와 컨테이너 구성

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'Javascript: The Good Parts' },
+        {title: 'Java Programming' },
+        {title: 'Python: The Good Parts' },
+        {title: 'Django: The Good Parts' }
+    ]
+}
